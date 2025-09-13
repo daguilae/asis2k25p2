8ca9e4dbdbf27b0f744430d21e461569a5b4e7f0
nelsonjose1 <nelsongodinez10m@gmail.com>
2025-09-12 22:20:12 -0600

    Modificando clases (Vista y Modelo)

---
+++ codigo/componentes/consultas/Componente_Consultas/Capa_Modelo_Componente_Consultas/Datos.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Odbc;
+
+namespace Capa_Modelo_Componente_Consultas
+{
+    public class Datos
+    {
+        public class UserQuery
+        {
+            public string Name { get; set; }
+            public string Sql { get; set; }
+            public override string ToString() => Name;
+        }
+    }
+}
