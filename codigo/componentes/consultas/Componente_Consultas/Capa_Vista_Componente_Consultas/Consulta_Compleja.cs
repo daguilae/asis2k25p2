6520ad6da571584aca32327a2913511fe91c9ec0
Diego Monterroso <149099355+Dmonterro@users.noreply.github.com>
2025-09-17 19:11:47 -0600

    Estadarizacion de la pestaña de consulta_compleja

--- codigo/componentes/consultas/Componente_Consultas/Capa_Vista_Componente_Consultas/Consulta_Compleja.cs
+++ codigo/componentes/consultas/Componente_Consultas/Capa_Vista_Componente_Consultas/Consulta_Compleja.cs
@@ -19,14 +19,14 @@ namespace Capa_Vista_Componente_Consultas
             InitializeComponent();
         }
 
-        private void btn_consimple_Click(object sender, EventArgs e)
+        private void Btn_consimple_Click(object sender, EventArgs e)
         {
             Frm_Consultas consultas = new Frm_Consultas();
             consultas.Show();
             this.Hide();
         }
 
-        private void btn_regresar_Click(object sender, EventArgs e)
+        private void Btn_regresar_Click(object sender, EventArgs e)
         {
             // Volvemos a mostrar Form1
             Frm_Principal principal = new Frm_Principal();
@@ -34,12 +34,12 @@ namespace Capa_Vista_Componente_Consultas
             this.Close(); // cerramos Form2
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void Btn_min_Click(object sender, EventArgs e)
         {
             this.WindowState = FormWindowState.Minimized;
         }
 
-        private void btn_max_Click(object sender, EventArgs e)
+        private void Btn_max_Click(object sender, EventArgs e)
         {
             if (this.WindowState == FormWindowState.Normal)
             {
@@ -49,6 +49,72 @@ namespace Capa_Vista_Componente_Consultas
             {
                 this.WindowState = FormWindowState.Normal; // Si ya está maximizada, se restaura
             }
+        }
+
+        private void Lbl_Tabla_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Consulta_Compleja_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Dgv_Sql_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Btn_buscar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Cbo_busqueda_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Gpb_Orden_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Rdb_asc_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void gpb_Orden_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Rdb_desc_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+    
+        private void Lst_Querys_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn_editar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn_eliminar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn_EjecutarSQL_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
