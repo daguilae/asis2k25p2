45322c09121595e724c3cbf9356cfd841bed43d6
anderson22t <atriguerosm3@miumg.edu.gt>
2025-10-15 15:27:50 -0600

    Creación de Proyecto de Clases y Ejecutable

---
+++ codigo/modulos/hoteleria/Modulo_Hoteleria/Capa_Modelo_Hoteleria/Cls_Conexion_Hoteleria.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capa_Modelo_Hoteleria
+{
+    class Cls_Conexion_Hoteleria
+    {
+    }
+}
---
+++ codigo/modulos/hoteleria/Modulo_Hoteleria/Capa_Modelo_Hoteleria/Cls_Sentencias_Habitaciones.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capa_Modelo_Hoteleria
+{
+    public class Cls_Sentencias_Habitaciones
+    {
+
+    }
+}
