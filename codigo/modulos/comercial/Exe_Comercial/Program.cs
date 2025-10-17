2e67147d639a5a6864ad9602a888c0680e6a08ec
Stevens Cambranes <cambranesstevens@gmail.com>
2025-10-16 20:22:56 -0600

    Estructurando mejor Capa Vista y haciendo referencias

--- codigo/modulos/comercial/Exe_Comercial/Program.cs
+++ codigo/modulos/comercial/Exe_Comercial/Program.cs
@@ -3,21 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using Capa_Vista_Modulo_Comercial.Compras;
+using Capa_Vista_Modulo_Comercial.Inventario;
 
 namespace Exe_Comercial
 {
     static class Program
     {
-        /// <summary>
-        /// Punto de entrada principal para la aplicación.
-        /// </summary>
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Frm_Compras());
+            Application.Run(new Frm_Inventario());
         }
     }
 }
