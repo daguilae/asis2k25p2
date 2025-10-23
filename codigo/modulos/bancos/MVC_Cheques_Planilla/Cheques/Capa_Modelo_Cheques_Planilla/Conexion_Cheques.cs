2a8b0d0c7c02db12d897fcaca1d2346048847337
Rocio López <guisell.alvarado@gmail.com>
2025-10-22 23:17:41 -0600

    Creacion de Modelo vista controlador de generación de cheques para planilla. Se agregaron clases de conexion, sentencias y controlador.

---
+++ codigo/modulos/bancos/MVC_Cheques_Planilla/Cheques/Capa_Modelo_Cheques_Planilla/Conexion_Cheques.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Odbc;
+
+
+namespace Capa_Modelo_Cheques_Planilla
+{
+    class Conexion_Cheques
+    {
+        public OdbcConnection ConexionBD()
+        {
+            OdbcConnection conn = new OdbcConnection("Dsn=bd_hoteleria");
+            try
+            {
+                conn.Open();
+                Console.WriteLine("Conexión exitosa.");
+            }
+            catch (OdbcException ex)
+            {
+                Console.WriteLine("Error al conectar: " + ex.Message);
+            }
+            return conn;
+        }
+
+        public void Desconexion(OdbcConnection conn)
+        {
+            try
+            {
+                conn.Close();
+                Console.WriteLine("Conexión cerrada.");
+            }
+            catch (OdbcException ex)
+            {
+                Console.WriteLine("Error al cerrar la conexión: " + ex.Message);
+            }
+        }
+    }
+}
