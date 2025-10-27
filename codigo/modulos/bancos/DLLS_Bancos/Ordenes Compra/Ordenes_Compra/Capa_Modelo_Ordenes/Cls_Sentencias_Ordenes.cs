9afe7d14d1423baec27e35aeaef03feb33a995d2
mariamoralesg42 <mmoralesg42@miumg.edu.gt>
2025-10-26 19:10:18 -0600

    Interfaz grafica y conexion con tabla para consultar, agregado de codigo MVC

--- codigo/modulos/bancos/DLLS_Bancos/Ordenes Compra/Ordenes_Compra/Capa_Modelo_Ordenes/Cls_Sentencias_Ordenes.cs
+++ codigo/modulos/bancos/DLLS_Bancos/Ordenes Compra/Ordenes_Compra/Capa_Modelo_Ordenes/Cls_Sentencias_Ordenes.cs
@@ -1,12 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data;
+using System.Data.Odbc;
 
 namespace Capa_Modelo_Ordenes
 {
     public class Cls_Sentencias_Ordenes
     {
+        Cls_Conexion_Ordenes con = new Cls_Conexion_Ordenes();
+
+        // Método para obtener todas las órdenes autorizadas con información completa
+        public OdbcDataAdapter llenarTbl(string tabla)// metodo  que obtinene el contenio de una tabla
+        {
+            //string para almacenar los campos de OBTENERCAMPOS y utilizar el 1ro
+            string sql = "SELECT * FROM " + tabla + "  ;";
+            OdbcDataAdapter dataTable = new OdbcDataAdapter(sql, con.conexion());
+            return dataTable;
+        }
     }
 }
