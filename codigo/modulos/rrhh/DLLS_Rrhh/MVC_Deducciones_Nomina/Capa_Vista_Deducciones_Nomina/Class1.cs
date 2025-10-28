8f3c919d8c28e200e206e287ae12e6e3a8751249
DanielReyes03 <daniel_reyes2003@outlook.com>
2025-10-27 23:10:18 -0600

    corrigiendo capa vista

--- codigo/modulos/rrhh/DLLS_Rrhh/MVC_Deducciones_Nomina/Capa_Vista_Deducciones_Nomina/Class1.cs
+++ codigo/modulos/rrhh/DLLS_Rrhh/MVC_Deducciones_Nomina/Capa_Vista_Deducciones_Nomina/Class1.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CapaVista
+namespace Capa_Vista_Deducciones_Nomina
 {
     public class Class1
     {
--- codigo/modulos/rrhh/DLLS_Rrhh/MVC_Deducciones_Nomina/Capa_Vista_Deducciones_Nomina/Properties/AssemblyInfo.cs
+++ codigo/modulos/rrhh/DLLS_Rrhh/MVC_Deducciones_Nomina/Capa_Vista_Deducciones_Nomina/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // La información general de un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
 // asociada con un ensamblado.
-[assembly: AssemblyTitle("CapaVista")]
+[assembly: AssemblyTitle("Capa_Vista_Deducciones_Nomina")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CapaVista")]
+[assembly: AssemblyProduct("Capa_Vista_Deducciones_Nomina")]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
-[assembly: Guid("82c7bed8-7934-486d-91f8-9fc02869e770")]
+[assembly: Guid("815b0360-528b-4bbc-b0fd-a16ff724c439")]
 
 // La información de versión de un ensamblado consta de los cuatro valores siguientes:
 //
