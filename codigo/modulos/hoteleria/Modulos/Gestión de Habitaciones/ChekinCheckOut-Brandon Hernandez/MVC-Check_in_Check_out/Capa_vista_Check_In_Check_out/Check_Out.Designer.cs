b7dfe458ea90236f7980b65b2571a8ee606c414d
BrandonHernandez2708 <157521682+BrandonHernandez2708@users.noreply.github.com>
2025-10-30 09:48:55 -0600

    Formulario areas funcionando con navegador

---
+++ codigo/modulos/hoteleria/Modulos/Gestión de Habitaciones/ChekinCheckOut-Brandon Hernandez/MVC-Check_in_Check_out/Capa_vista_Check_In_Check_out/Check_Out.Designer.cs
@@ -0,0 +1,311 @@
+﻿
+namespace Capa_vista_Check_In_Check_out
+{
+    partial class Check_Out
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Gbp_opc = new System.Windows.Forms.GroupBox();
+            this.Btn_Eliminar = new System.Windows.Forms.Button();
+            this.Btn_salir = new System.Windows.Forms.Button();
+            this.Btn_nuevo = new System.Windows.Forms.Button();
+            this.Btn_cancelar = new System.Windows.Forms.Button();
+            this.Btn_modificar = new System.Windows.Forms.Button();
+            this.Btn_guardar = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Gbp_Titulo = new System.Windows.Forms.GroupBox();
+            this.Lbl_titulo = new System.Windows.Forms.Label();
+            this.Gbp_Campos = new System.Windows.Forms.GroupBox();
+            this.Cbo_Huesped = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Lbl_Fecha = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.Lbl_idreserva = new System.Windows.Forms.Label();
+            this.Lbl_IdcheckOut = new System.Windows.Forms.Label();
+            this.Lbl_Icheck_in = new System.Windows.Forms.Label();
+            this.Gbp_opc.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.Gbp_Titulo.SuspendLayout();
+            this.Gbp_Campos.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Gbp_opc
+            // 
+            this.Gbp_opc.Controls.Add(this.Btn_Eliminar);
+            this.Gbp_opc.Controls.Add(this.Btn_salir);
+            this.Gbp_opc.Controls.Add(this.Btn_nuevo);
+            this.Gbp_opc.Controls.Add(this.Btn_cancelar);
+            this.Gbp_opc.Controls.Add(this.Btn_modificar);
+            this.Gbp_opc.Controls.Add(this.Btn_guardar);
+            this.Gbp_opc.Font = new System.Drawing.Font("Rockwell", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Gbp_opc.Location = new System.Drawing.Point(1163, 285);
+            this.Gbp_opc.Name = "Gbp_opc";
+            this.Gbp_opc.Size = new System.Drawing.Size(227, 472);
+            this.Gbp_opc.TabIndex = 16;
+            this.Gbp_opc.TabStop = false;
+            this.Gbp_opc.Text = "Opciones";
+            // 
+            // Btn_Eliminar
+            // 
+            this.Btn_Eliminar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(61)))), ((int)(((byte)(78)))), ((int)(((byte)(88)))));
+            this.Btn_Eliminar.FlatAppearance.BorderColor = System.Drawing.SystemColors.ButtonShadow;
+            this.Btn_Eliminar.Font = new System.Drawing.Font("Rockwell", 10F);
+            this.Btn_Eliminar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(201)))), ((int)(((byte)(214)))), ((int)(((byte)(221)))));
+            this.Btn_Eliminar.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.Btn_Eliminar.Location = new System.Drawing.Point(30, 241);
+            this.Btn_Eliminar.Name = "Btn_Eliminar";
+            this.Btn_Eliminar.Size = new System.Drawing.Size(143, 54);
+            this.Btn_Eliminar.TabIndex = 5;
+            this.Btn_Eliminar.Text = "Eliminar";
+            this.Btn_Eliminar.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Btn_Eliminar.UseVisualStyleBackColor = false;
+            // 
+            // Btn_salir
+            // 
+            this.Btn_salir.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(61)))), ((int)(((byte)(78)))), ((int)(((byte)(88)))));
+            this.Btn_salir.FlatAppearance.BorderColor = System.Drawing.SystemColors.ButtonShadow;
+            this.Btn_salir.Font = new System.Drawing.Font("Rockwell", 10F);
+            this.Btn_salir.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(201)))), ((int)(((byte)(214)))), ((int)(((byte)(221)))));
+            this.Btn_salir.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.Btn_salir.Location = new System.Drawing.Point(30, 381);
+            this.Btn_salir.Name = "Btn_salir";
+            this.Btn_salir.Size = new System.Drawing.Size(143, 54);
+            this.Btn_salir.TabIndex = 4;
+            this.Btn_salir.Text = "Salir";
+            this.Btn_salir.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Btn_salir.UseVisualStyleBackColor = false;
+            // 
+            // Btn_nuevo
+            // 
+            this.Btn_nuevo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(61)))), ((int)(((byte)(78)))), ((int)(((byte)(88)))));
+            this.Btn_nuevo.FlatAppearance.BorderColor = System.Drawing.SystemColors.ButtonShadow;
+            this.Btn_nuevo.Font = new System.Drawing.Font("Rockwell", 10F);
+            this.Btn_nuevo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(201)))), ((int)(((byte)(214)))), ((int)(((byte)(221)))));
+            this.Btn_nuevo.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.Btn_nuevo.Location = new System.Drawing.Point(30, 171);
+            this.Btn_nuevo.Name = "Btn_nuevo";
+            this.Btn_nuevo.Size = new System.Drawing.Size(143, 54);
+            this.Btn_nuevo.TabIndex = 1;
+            this.Btn_nuevo.Text = "Nuevo";
+            this.Btn_nuevo.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Btn_nuevo.UseVisualStyleBackColor = false;
+            // 
+            // Btn_cancelar
+            // 
+            this.Btn_cancelar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(61)))), ((int)(((byte)(78)))), ((int)(((byte)(88)))));
+            this.Btn_cancelar.FlatAppearance.BorderColor = System.Drawing.SystemColors.ButtonShadow;
+            this.Btn_cancelar.Font = new System.Drawing.Font("Rockwell", 10F);
+            this.Btn_cancelar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(201)))), ((int)(((byte)(214)))), ((int)(((byte)(221)))));
+            this.Btn_cancelar.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.Btn_cancelar.Location = new System.Drawing.Point(30, 311);
+            this.Btn_cancelar.Name = "Btn_cancelar";
+            this.Btn_cancelar.Size = new System.Drawing.Size(143, 54);
+            this.Btn_cancelar.TabIndex = 3;
+            this.Btn_cancelar.Text = "Cancelar";
+            this.Btn_cancelar.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Btn_cancelar.UseVisualStyleBackColor = false;
+            // 
+            // Btn_modificar
+            // 
+            this.Btn_modificar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(61)))), ((int)(((byte)(78)))), ((int)(((byte)(88)))));
+            this.Btn_modificar.FlatAppearance.BorderColor = System.Drawing.SystemColors.ButtonShadow;
+            this.Btn_modificar.Font = new System.Drawing.Font("Rockwell", 10F);
+            this.Btn_modificar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(201)))), ((int)(((byte)(214)))), ((int)(((byte)(221)))));
+            this.Btn_modificar.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.Btn_modificar.Location = new System.Drawing.Point(30, 26);
+            this.Btn_modificar.Name = "Btn_modificar";
+            this.Btn_modificar.Size = new System.Drawing.Size(143, 54);
+            this.Btn_modificar.TabIndex = 2;
+            this.Btn_modificar.Text = "Modificar";
+            this.Btn_modificar.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Btn_modificar.UseVisualStyleBackColor = false;
+            // 
+            // Btn_guardar
+            // 
+            this.Btn_guardar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(61)))), ((int)(((byte)(78)))), ((int)(((byte)(88)))));
+            this.Btn_guardar.FlatAppearance.BorderColor = System.Drawing.SystemColors.ButtonShadow;
+            this.Btn_guardar.Font = new System.Drawing.Font("Rockwell", 10F);
+            this.Btn_guardar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(201)))), ((int)(((byte)(214)))), ((int)(((byte)(221)))));
+            this.Btn_guardar.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.Btn_guardar.Location = new System.Drawing.Point(30, 97);
+            this.Btn_guardar.Name = "Btn_guardar";
+            this.Btn_guardar.Size = new System.Drawing.Size(143, 54);
+            this.Btn_guardar.TabIndex = 0;
+            this.Btn_guardar.Text = "Guardar";
+            this.Btn_guardar.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Btn_guardar.UseVisualStyleBackColor = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(27, 285);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1120, 478);
+            this.dataGridView1.TabIndex = 15;
+            // 
+            // Gbp_Titulo
+            // 
+            this.Gbp_Titulo.AccessibleRole = System.Windows.Forms.AccessibleRole.TitleBar;
+            this.Gbp_Titulo.Controls.Add(this.Lbl_titulo);
+            this.Gbp_Titulo.Location = new System.Drawing.Point(434, 12);
+            this.Gbp_Titulo.Name = "Gbp_Titulo";
+            this.Gbp_Titulo.Size = new System.Drawing.Size(366, 80);
+            this.Gbp_Titulo.TabIndex = 14;
+            this.Gbp_Titulo.TabStop = false;
+            // 
+            // Lbl_titulo
+            // 
+            this.Lbl_titulo.AutoSize = true;
+            this.Lbl_titulo.BackColor = System.Drawing.SystemColors.Control;
+            this.Lbl_titulo.Font = new System.Drawing.Font("Rockwell", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_titulo.Location = new System.Drawing.Point(110, 33);
+            this.Lbl_titulo.Name = "Lbl_titulo";
+            this.Lbl_titulo.Size = new System.Drawing.Size(104, 21);
+            this.Lbl_titulo.TabIndex = 0;
+            this.Lbl_titulo.Text = "Check Out";
+            // 
+            // Gbp_Campos
+            // 
+            this.Gbp_Campos.Controls.Add(this.Cbo_Huesped);
+            this.Gbp_Campos.Controls.Add(this.textBox1);
+            this.Gbp_Campos.Controls.Add(this.Lbl_Fecha);
+            this.Gbp_Campos.Controls.Add(this.dateTimePicker1);
+            this.Gbp_Campos.Controls.Add(this.Lbl_idreserva);
+            this.Gbp_Campos.Controls.Add(this.Lbl_IdcheckOut);
+            this.Gbp_Campos.Controls.Add(this.Lbl_Icheck_in);
+            this.Gbp_Campos.Location = new System.Drawing.Point(27, 98);
+            this.Gbp_Campos.Name = "Gbp_Campos";
+            this.Gbp_Campos.Size = new System.Drawing.Size(1309, 170);
+            this.Gbp_Campos.TabIndex = 13;
+            this.Gbp_Campos.TabStop = false;
+            // 
+            // Cbo_Huesped
+            // 
+            this.Cbo_Huesped.FormattingEnabled = true;
+            this.Cbo_Huesped.Location = new System.Drawing.Point(232, 76);
+            this.Cbo_Huesped.Name = "Cbo_Huesped";
+            this.Cbo_Huesped.Size = new System.Drawing.Size(695, 24);
+            this.Cbo_Huesped.TabIndex = 8;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(37, 80);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(162, 22);
+            this.textBox1.TabIndex = 7;
+            // 
+            // Lbl_Fecha
+            // 
+            this.Lbl_Fecha.AutoSize = true;
+            this.Lbl_Fecha.Font = new System.Drawing.Font("Rockwell", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_Fecha.Location = new System.Drawing.Point(1046, 41);
+            this.Lbl_Fecha.Name = "Lbl_Fecha";
+            this.Lbl_Fecha.Size = new System.Drawing.Size(56, 20);
+            this.Lbl_Fecha.TabIndex = 5;
+            this.Lbl_Fecha.Text = "Fecha";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(957, 80);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(257, 22);
+            this.dateTimePicker1.TabIndex = 4;
+            // 
+            // Lbl_idreserva
+            // 
+            this.Lbl_idreserva.AutoSize = true;
+            this.Lbl_idreserva.Font = new System.Drawing.Font("Rockwell", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_idreserva.Location = new System.Drawing.Point(530, 29);
+            this.Lbl_idreserva.Name = "Lbl_idreserva";
+            this.Lbl_idreserva.Size = new System.Drawing.Size(0, 20);
+            this.Lbl_idreserva.TabIndex = 3;
+            // 
+            // Lbl_IdcheckOut
+            // 
+            this.Lbl_IdcheckOut.AutoSize = true;
+            this.Lbl_IdcheckOut.Font = new System.Drawing.Font("Rockwell", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_IdcheckOut.Location = new System.Drawing.Point(47, 41);
+            this.Lbl_IdcheckOut.Name = "Lbl_IdcheckOut";
+            this.Lbl_IdcheckOut.Size = new System.Drawing.Size(109, 20);
+            this.Lbl_IdcheckOut.TabIndex = 1;
+            this.Lbl_IdcheckOut.Text = "Id Check out";
+            // 
+            // Lbl_Icheck_in
+            // 
+            this.Lbl_Icheck_in.AutoSize = true;
+            this.Lbl_Icheck_in.Font = new System.Drawing.Font("Rockwell", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_Icheck_in.Location = new System.Drawing.Point(501, 41);
+            this.Lbl_Icheck_in.Name = "Lbl_Icheck_in";
+            this.Lbl_Icheck_in.Size = new System.Drawing.Size(80, 20);
+            this.Lbl_Icheck_in.TabIndex = 2;
+            this.Lbl_Icheck_in.Text = "Check In";
+            // 
+            // Check_Out
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1372, 788);
+            this.Controls.Add(this.Gbp_opc);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.Gbp_Titulo);
+            this.Controls.Add(this.Gbp_Campos);
+            this.Name = "Check_Out";
+            this.Text = "Check_Out";
+            this.Gbp_opc.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.Gbp_Titulo.ResumeLayout(false);
+            this.Gbp_Titulo.PerformLayout();
+            this.Gbp_Campos.ResumeLayout(false);
+            this.Gbp_Campos.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox Gbp_opc;
+        private System.Windows.Forms.Button Btn_Eliminar;
+        private System.Windows.Forms.Button Btn_salir;
+        private System.Windows.Forms.Button Btn_nuevo;
+        private System.Windows.Forms.Button Btn_cancelar;
+        private System.Windows.Forms.Button Btn_modificar;
+        private System.Windows.Forms.Button Btn_guardar;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox Gbp_Titulo;
+        private System.Windows.Forms.Label Lbl_titulo;
+        private System.Windows.Forms.GroupBox Gbp_Campos;
+        private System.Windows.Forms.ComboBox Cbo_Huesped;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label Lbl_Fecha;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label Lbl_idreserva;
+        private System.Windows.Forms.Label Lbl_IdcheckOut;
+        private System.Windows.Forms.Label Lbl_Icheck_in;
+    }
+}
