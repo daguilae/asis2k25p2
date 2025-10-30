be4f2291746577b9923e3e88729ae2d809e310fa
Rocio López <guisell.alvarado@gmail.com>
2025-10-29 18:41:22 -0600

    Creacion de Dll de cheques planilla

---
+++ codigo/modulos/bancos/DLLS_Bancos/Cheques/Capa_Controlador_Cheques_Planilla/Capa_Controlador_Cheques_Planilla.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Capa_Modelo_Cheques_Planilla;
+
+namespace Capa_Controlador_Cheques_Planilla
+{
+   public class Capa_Controlador_Cheques_Planilla
+    {
+        Sentencias_Cheques_Planillas sn = new Sentencias_Cheques_Planillas();
+        
+       
+        //9959-23-740
+    }
+}
---
+++ codigo/modulos/bancos/DLLS_Bancos/Cheques/Capa_Modelo_Cheques_Planilla/Sentencias_Cheques_Planillas.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Odbc;
+using System.Data;
+
+namespace Capa_Modelo_Cheques_Planilla
+{
+   public class Sentencias_Cheques_Planillas
+    {
+        Conexion_Cheques con = new Conexion_Cheques();
+
+
+
+
+
+    }
+}
