ea3de1766a8dc937aeecb94fc6ddebf5a67621cd
Carlo <andilucas30@gmail.com>
2025-10-30 23:26:16 -0600

    Agregando anticipos

--- codigo/modulos/rrhh/DLLS_Rrhh/MVC_Anticipos_Nominas/Capa_Vista_Anticipos/Anticipos.Designer.cs
+++ codigo/modulos/rrhh/DLLS_Rrhh/MVC_Anticipos_Nominas/Capa_Vista_Anticipos/Anticipos.Designer.cs
@@ -31,14 +31,14 @@ namespace Capa_Vista_Anticipos
         {
             this.Gpb_anticipos = new System.Windows.Forms.GroupBox();
             this.Lbl_NombreEmpleado = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.Dgv_anticipo = new System.Windows.Forms.DataGridView();
             this.Btn_buscar = new System.Windows.Forms.Button();
             this.Btn_solicitar = new System.Windows.Forms.Button();
             this.Btn_modificar = new System.Windows.Forms.Button();
             this.Btn_eliminar = new System.Windows.Forms.Button();
+            this.Dgv_anticipo = new System.Windows.Forms.DataGridView();
             this.Pnl_Encabezado = new System.Windows.Forms.Panel();
             this.Lbl_Titulo = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.Gpb_anticipos.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.Dgv_anticipo)).BeginInit();
             this.Pnl_Encabezado.SuspendLayout();
@@ -46,19 +46,17 @@ namespace Capa_Vista_Anticipos
             // 
             // Gpb_anticipos
             // 
+            this.Gpb_anticipos.Controls.Add(this.textBox1);
             this.Gpb_anticipos.Controls.Add(this.Lbl_NombreEmpleado);
-            this.Gpb_anticipos.Controls.Add(this.comboBox1);
             this.Gpb_anticipos.Controls.Add(this.Btn_buscar);
             this.Gpb_anticipos.Controls.Add(this.Btn_solicitar);
             this.Gpb_anticipos.Controls.Add(this.Btn_modificar);
             this.Gpb_anticipos.Controls.Add(this.Btn_eliminar);
             this.Gpb_anticipos.Controls.Add(this.Dgv_anticipo);
             this.Gpb_anticipos.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
-            this.Gpb_anticipos.Location = new System.Drawing.Point(85, 118);
-            this.Gpb_anticipos.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Gpb_anticipos.Location = new System.Drawing.Point(57, 77);
             this.Gpb_anticipos.Name = "Gpb_anticipos";
-            this.Gpb_anticipos.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.Gpb_anticipos.Size = new System.Drawing.Size(1244, 717);
+            this.Gpb_anticipos.Size = new System.Drawing.Size(829, 466);
             this.Gpb_anticipos.TabIndex = 8;
             this.Gpb_anticipos.TabStop = false;
             this.Gpb_anticipos.Text = "Anticipos";
@@ -67,40 +65,19 @@ namespace Capa_Vista_Anticipos
             // 
             this.Lbl_NombreEmpleado.AutoSize = true;
             this.Lbl_NombreEmpleado.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
-            this.Lbl_NombreEmpleado.Location = new System.Drawing.Point(15, 86);
-            this.Lbl_NombreEmpleado.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Lbl_NombreEmpleado.Location = new System.Drawing.Point(10, 56);
             this.Lbl_NombreEmpleado.Name = "Lbl_NombreEmpleado";
-            this.Lbl_NombreEmpleado.Size = new System.Drawing.Size(317, 40);
+            this.Lbl_NombreEmpleado.Size = new System.Drawing.Size(218, 29);
             this.Lbl_NombreEmpleado.TabIndex = 1;
             this.Lbl_NombreEmpleado.Text = "Nombre Empleado";
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(376, 82);
-            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(316, 48);
-            this.comboBox1.TabIndex = 2;
-            // 
-            // Dgv_anticipo
-            // 
-            this.Dgv_anticipo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.Dgv_anticipo.Location = new System.Drawing.Point(22, 149);
-            this.Dgv_anticipo.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.Dgv_anticipo.Name = "Dgv_anticipo";
-            this.Dgv_anticipo.RowHeadersWidth = 62;
-            this.Dgv_anticipo.Size = new System.Drawing.Size(1164, 489);
-            this.Dgv_anticipo.TabIndex = 0;
             // 
             // Btn_buscar
             // 
             this.Btn_buscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
             this.Btn_buscar.Image = global::Capa_Vista_Anticipos.Properties.Resources.android_search_icon_icons_com_50501;
-            this.Btn_buscar.Location = new System.Drawing.Point(722, 43);
-            this.Btn_buscar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Btn_buscar.Location = new System.Drawing.Point(481, 28);
             this.Btn_buscar.Name = "Btn_buscar";
-            this.Btn_buscar.Size = new System.Drawing.Size(106, 97);
+            this.Btn_buscar.Size = new System.Drawing.Size(71, 63);
             this.Btn_buscar.TabIndex = 3;
             this.Btn_buscar.Text = "Buscar";
             this.Btn_buscar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
@@ -110,10 +87,9 @@ namespace Capa_Vista_Anticipos
             // 
             this.Btn_solicitar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
             this.Btn_solicitar.Image = global::Capa_Vista_Anticipos.Properties.Resources.add_insert_new_plus_button_icon_142943;
-            this.Btn_solicitar.Location = new System.Drawing.Point(837, 43);
-            this.Btn_solicitar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Btn_solicitar.Location = new System.Drawing.Point(558, 28);
             this.Btn_solicitar.Name = "Btn_solicitar";
-            this.Btn_solicitar.Size = new System.Drawing.Size(106, 97);
+            this.Btn_solicitar.Size = new System.Drawing.Size(71, 63);
             this.Btn_solicitar.TabIndex = 4;
             this.Btn_solicitar.Text = "Solicitar";
             this.Btn_solicitar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
@@ -124,10 +100,9 @@ namespace Capa_Vista_Anticipos
             // 
             this.Btn_modificar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
             this.Btn_modificar.Image = global::Capa_Vista_Anticipos.Properties.Resources.compose_edit_modify_icon_177770;
-            this.Btn_modificar.Location = new System.Drawing.Point(952, 43);
-            this.Btn_modificar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Btn_modificar.Location = new System.Drawing.Point(635, 28);
             this.Btn_modificar.Name = "Btn_modificar";
-            this.Btn_modificar.Size = new System.Drawing.Size(118, 97);
+            this.Btn_modificar.Size = new System.Drawing.Size(79, 63);
             this.Btn_modificar.TabIndex = 5;
             this.Btn_modificar.Text = "Modificar";
             this.Btn_modificar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
@@ -138,14 +113,22 @@ namespace Capa_Vista_Anticipos
             // 
             this.Btn_eliminar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
             this.Btn_eliminar.Image = global::Capa_Vista_Anticipos.Properties.Resources.delete_remove_trash_icon_177304;
-            this.Btn_eliminar.Location = new System.Drawing.Point(1080, 43);
-            this.Btn_eliminar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Btn_eliminar.Location = new System.Drawing.Point(720, 28);
             this.Btn_eliminar.Name = "Btn_eliminar";
-            this.Btn_eliminar.Size = new System.Drawing.Size(106, 97);
+            this.Btn_eliminar.Size = new System.Drawing.Size(71, 63);
             this.Btn_eliminar.TabIndex = 6;
             this.Btn_eliminar.Text = "Eliminar";
             this.Btn_eliminar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
             this.Btn_eliminar.UseVisualStyleBackColor = true;
+            // 
+            // Dgv_anticipo
+            // 
+            this.Dgv_anticipo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Dgv_anticipo.Location = new System.Drawing.Point(15, 97);
+            this.Dgv_anticipo.Name = "Dgv_anticipo";
+            this.Dgv_anticipo.RowHeadersWidth = 62;
+            this.Dgv_anticipo.Size = new System.Drawing.Size(776, 318);
+            this.Dgv_anticipo.TabIndex = 0;
             // 
             // Pnl_Encabezado
             // 
@@ -153,27 +136,37 @@ namespace Capa_Vista_Anticipos
             this.Pnl_Encabezado.Controls.Add(this.Lbl_Titulo);
             this.Pnl_Encabezado.Dock = System.Windows.Forms.DockStyle.Top;
             this.Pnl_Encabezado.Location = new System.Drawing.Point(0, 0);
+            this.Pnl_Encabezado.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Pnl_Encabezado.Name = "Pnl_Encabezado";
-            this.Pnl_Encabezado.Size = new System.Drawing.Size(1381, 96);
+            this.Pnl_Encabezado.Size = new System.Drawing.Size(921, 62);
             this.Pnl_Encabezado.TabIndex = 13;
             // 
             // Lbl_Titulo
             // 
             this.Lbl_Titulo.AutoSize = true;
             this.Lbl_Titulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
-            this.Lbl_Titulo.Location = new System.Drawing.Point(91, 23);
+            this.Lbl_Titulo.Location = new System.Drawing.Point(61, 15);
+            this.Lbl_Titulo.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.Lbl_Titulo.Name = "Lbl_Titulo";
-            this.Lbl_Titulo.Size = new System.Drawing.Size(163, 40);
+            this.Lbl_Titulo.Size = new System.Drawing.Size(111, 29);
             this.Lbl_Titulo.TabIndex = 0;
             this.Lbl_Titulo.Text = "Anticipos";
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(234, 53);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(241, 35);
+            this.textBox1.TabIndex = 7;
+            // 
             // Anticipos
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1381, 862);
+            this.ClientSize = new System.Drawing.Size(921, 560);
             this.Controls.Add(this.Pnl_Encabezado);
             this.Controls.Add(this.Gpb_anticipos);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Anticipos";
             this.Text = "Anticipos";
             this.Gpb_anticipos.ResumeLayout(false);
@@ -189,7 +182,6 @@ namespace Capa_Vista_Anticipos
 
         private System.Windows.Forms.GroupBox Gpb_anticipos;
         private System.Windows.Forms.Label Lbl_NombreEmpleado;
-        private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Button Btn_buscar;
         private System.Windows.Forms.Button Btn_solicitar;
         private System.Windows.Forms.Button Btn_modificar;
@@ -197,5 +189,6 @@ namespace Capa_Vista_Anticipos
         private System.Windows.Forms.DataGridView Dgv_anticipo;
         private System.Windows.Forms.Panel Pnl_Encabezado;
         private System.Windows.Forms.Label Lbl_Titulo;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
--- codigo/modulos/rrhh/DLLS_Rrhh/MVC_Anticipos_Nominas/Capa_Vista_Anticipos/solicitaranticipo.Designer.cs
+++ codigo/modulos/rrhh/DLLS_Rrhh/MVC_Anticipos_Nominas/Capa_Vista_Anticipos/solicitaranticipo.Designer.cs
@@ -30,18 +30,18 @@ namespace Capa_Vista_Anticipos
         private void InitializeComponent()
         {
             this.Pnl_Encabezado = new System.Windows.Forms.Panel();
+            this.Lbl_Titulo = new System.Windows.Forms.Label();
             this.Gpb_solicitaranticipo = new System.Windows.Forms.GroupBox();
             this.Btn_Cancelar = new System.Windows.Forms.Button();
             this.Btn_guardar = new System.Windows.Forms.Button();
             this.Txt_motivo = new System.Windows.Forms.TextBox();
             this.Txt_monto = new System.Windows.Forms.TextBox();
             this.Dtp_fecha = new System.Windows.Forms.DateTimePicker();
-            this.Cbo_Nombreempleados = new System.Windows.Forms.ComboBox();
             this.Lbl_motivo = new System.Windows.Forms.Label();
             this.Lbl_monto = new System.Windows.Forms.Label();
             this.Lbl_fecha = new System.Windows.Forms.Label();
             this.Lbl_Nombreempleado = new System.Windows.Forms.Label();
-            this.Lbl_Titulo = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.Pnl_Encabezado.SuspendLayout();
             this.Gpb_solicitaranticipo.SuspendLayout();
             this.SuspendLayout();
@@ -52,28 +52,38 @@ namespace Capa_Vista_Anticipos
             this.Pnl_Encabezado.Controls.Add(this.Lbl_Titulo);
             this.Pnl_Encabezado.Dock = System.Windows.Forms.DockStyle.Top;
             this.Pnl_Encabezado.Location = new System.Drawing.Point(0, 0);
+            this.Pnl_Encabezado.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Pnl_Encabezado.Name = "Pnl_Encabezado";
-            this.Pnl_Encabezado.Size = new System.Drawing.Size(1335, 96);
+            this.Pnl_Encabezado.Size = new System.Drawing.Size(890, 62);
             this.Pnl_Encabezado.TabIndex = 12;
+            // 
+            // Lbl_Titulo
+            // 
+            this.Lbl_Titulo.AutoSize = true;
+            this.Lbl_Titulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.Lbl_Titulo.Location = new System.Drawing.Point(61, 15);
+            this.Lbl_Titulo.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Lbl_Titulo.Name = "Lbl_Titulo";
+            this.Lbl_Titulo.Size = new System.Drawing.Size(111, 29);
+            this.Lbl_Titulo.TabIndex = 0;
+            this.Lbl_Titulo.Text = "Anticipos";
             // 
             // Gpb_solicitaranticipo
             // 
+            this.Gpb_solicitaranticipo.Controls.Add(this.textBox1);
             this.Gpb_solicitaranticipo.Controls.Add(this.Btn_Cancelar);
             this.Gpb_solicitaranticipo.Controls.Add(this.Btn_guardar);
             this.Gpb_solicitaranticipo.Controls.Add(this.Txt_motivo);
             this.Gpb_solicitaranticipo.Controls.Add(this.Txt_monto);
             this.Gpb_solicitaranticipo.Controls.Add(this.Dtp_fecha);
-            this.Gpb_solicitaranticipo.Controls.Add(this.Cbo_Nombreempleados);
             this.Gpb_solicitaranticipo.Controls.Add(this.Lbl_motivo);
             this.Gpb_solicitaranticipo.Controls.Add(this.Lbl_monto);
             this.Gpb_solicitaranticipo.Controls.Add(this.Lbl_fecha);
             this.Gpb_solicitaranticipo.Controls.Add(this.Lbl_Nombreempleado);
             this.Gpb_solicitaranticipo.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
-            this.Gpb_solicitaranticipo.Location = new System.Drawing.Point(73, 110);
-            this.Gpb_solicitaranticipo.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Gpb_solicitaranticipo.Location = new System.Drawing.Point(49, 71);
             this.Gpb_solicitaranticipo.Name = "Gpb_solicitaranticipo";
-            this.Gpb_solicitaranticipo.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.Gpb_solicitaranticipo.Size = new System.Drawing.Size(1222, 726);
+            this.Gpb_solicitaranticipo.Size = new System.Drawing.Size(815, 472);
             this.Gpb_solicitaranticipo.TabIndex = 13;
             this.Gpb_solicitaranticipo.TabStop = false;
             this.Gpb_solicitaranticipo.Text = "Solicitar Anticipo";
@@ -82,10 +92,9 @@ namespace Capa_Vista_Anticipos
             // 
             this.Btn_Cancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
             this.Btn_Cancelar.Image = global::Capa_Vista_Anticipos.Properties.Resources.Cancel_icon_icons_com_73703;
-            this.Btn_Cancelar.Location = new System.Drawing.Point(686, 594);
-            this.Btn_Cancelar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Btn_Cancelar.Location = new System.Drawing.Point(457, 386);
             this.Btn_Cancelar.Name = "Btn_Cancelar";
-            this.Btn_Cancelar.Size = new System.Drawing.Size(120, 97);
+            this.Btn_Cancelar.Size = new System.Drawing.Size(80, 63);
             this.Btn_Cancelar.TabIndex = 11;
             this.Btn_Cancelar.Text = "Cancelar";
             this.Btn_Cancelar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
@@ -96,10 +105,9 @@ namespace Capa_Vista_Anticipos
             // 
             this.Btn_guardar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
             this.Btn_guardar.Image = global::Capa_Vista_Anticipos.Properties.Resources.savetheapplication_guardar_2958;
-            this.Btn_guardar.Location = new System.Drawing.Point(414, 594);
-            this.Btn_guardar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Btn_guardar.Location = new System.Drawing.Point(276, 386);
             this.Btn_guardar.Name = "Btn_guardar";
-            this.Btn_guardar.Size = new System.Drawing.Size(106, 97);
+            this.Btn_guardar.Size = new System.Drawing.Size(71, 63);
             this.Btn_guardar.TabIndex = 10;
             this.Btn_guardar.Text = "Guardar";
             this.Btn_guardar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
@@ -108,49 +116,35 @@ namespace Capa_Vista_Anticipos
             // Txt_motivo
             // 
             this.Txt_motivo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.Txt_motivo.Location = new System.Drawing.Point(414, 312);
-            this.Txt_motivo.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Txt_motivo.Location = new System.Drawing.Point(276, 203);
             this.Txt_motivo.Multiline = true;
             this.Txt_motivo.Name = "Txt_motivo";
-            this.Txt_motivo.Size = new System.Drawing.Size(376, 210);
+            this.Txt_motivo.Size = new System.Drawing.Size(252, 138);
             this.Txt_motivo.TabIndex = 9;
             // 
             // Txt_monto
             // 
             this.Txt_monto.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.Txt_monto.Location = new System.Drawing.Point(414, 237);
-            this.Txt_monto.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Txt_monto.Location = new System.Drawing.Point(276, 154);
             this.Txt_monto.Name = "Txt_monto";
-            this.Txt_monto.Size = new System.Drawing.Size(376, 30);
+            this.Txt_monto.Size = new System.Drawing.Size(252, 23);
             this.Txt_monto.TabIndex = 8;
             // 
             // Dtp_fecha
             // 
             this.Dtp_fecha.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.Dtp_fecha.Location = new System.Drawing.Point(414, 178);
-            this.Dtp_fecha.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Dtp_fecha.Location = new System.Drawing.Point(276, 116);
             this.Dtp_fecha.Name = "Dtp_fecha";
-            this.Dtp_fecha.Size = new System.Drawing.Size(376, 30);
+            this.Dtp_fecha.Size = new System.Drawing.Size(252, 23);
             this.Dtp_fecha.TabIndex = 7;
-            // 
-            // Cbo_Nombreempleados
-            // 
-            this.Cbo_Nombreempleados.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.Cbo_Nombreempleados.FormattingEnabled = true;
-            this.Cbo_Nombreempleados.Location = new System.Drawing.Point(414, 112);
-            this.Cbo_Nombreempleados.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.Cbo_Nombreempleados.Name = "Cbo_Nombreempleados";
-            this.Cbo_Nombreempleados.Size = new System.Drawing.Size(376, 33);
-            this.Cbo_Nombreempleados.TabIndex = 6;
             // 
             // Lbl_motivo
             // 
             this.Lbl_motivo.AutoSize = true;
             this.Lbl_motivo.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
-            this.Lbl_motivo.Location = new System.Drawing.Point(69, 312);
-            this.Lbl_motivo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Lbl_motivo.Location = new System.Drawing.Point(46, 203);
             this.Lbl_motivo.Name = "Lbl_motivo";
-            this.Lbl_motivo.Size = new System.Drawing.Size(123, 40);
+            this.Lbl_motivo.Size = new System.Drawing.Size(84, 29);
             this.Lbl_motivo.TabIndex = 5;
             this.Lbl_motivo.Text = "Motivo";
             // 
@@ -158,10 +152,9 @@ namespace Capa_Vista_Anticipos
             // 
             this.Lbl_monto.AutoSize = true;
             this.Lbl_monto.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
-            this.Lbl_monto.Location = new System.Drawing.Point(69, 242);
-            this.Lbl_monto.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Lbl_monto.Location = new System.Drawing.Point(46, 157);
             this.Lbl_monto.Name = "Lbl_monto";
-            this.Lbl_monto.Size = new System.Drawing.Size(117, 40);
+            this.Lbl_monto.Size = new System.Drawing.Size(80, 29);
             this.Lbl_monto.TabIndex = 4;
             this.Lbl_monto.Text = "Monto";
             // 
@@ -169,10 +162,9 @@ namespace Capa_Vista_Anticipos
             // 
             this.Lbl_fecha.AutoSize = true;
             this.Lbl_fecha.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
-            this.Lbl_fecha.Location = new System.Drawing.Point(69, 172);
-            this.Lbl_fecha.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Lbl_fecha.Location = new System.Drawing.Point(46, 112);
             this.Lbl_fecha.Name = "Lbl_fecha";
-            this.Lbl_fecha.Size = new System.Drawing.Size(117, 40);
+            this.Lbl_fecha.Size = new System.Drawing.Size(80, 29);
             this.Lbl_fecha.TabIndex = 3;
             this.Lbl_fecha.Text = "Fecha";
             // 
@@ -180,30 +172,28 @@ namespace Capa_Vista_Anticipos
             // 
             this.Lbl_Nombreempleado.AutoSize = true;
             this.Lbl_Nombreempleado.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
-            this.Lbl_Nombreempleado.Location = new System.Drawing.Point(69, 108);
-            this.Lbl_Nombreempleado.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Lbl_Nombreempleado.Location = new System.Drawing.Point(46, 70);
             this.Lbl_Nombreempleado.Name = "Lbl_Nombreempleado";
-            this.Lbl_Nombreempleado.Size = new System.Drawing.Size(317, 40);
+            this.Lbl_Nombreempleado.Size = new System.Drawing.Size(218, 29);
             this.Lbl_Nombreempleado.TabIndex = 2;
             this.Lbl_Nombreempleado.Text = "Nombre Empleado";
             // 
-            // Lbl_Titulo
+            // textBox1
             // 
-            this.Lbl_Titulo.AutoSize = true;
-            this.Lbl_Titulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
-            this.Lbl_Titulo.Location = new System.Drawing.Point(91, 23);
-            this.Lbl_Titulo.Name = "Lbl_Titulo";
-            this.Lbl_Titulo.Size = new System.Drawing.Size(163, 40);
-            this.Lbl_Titulo.TabIndex = 0;
-            this.Lbl_Titulo.Text = "Anticipos";
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.textBox1.Location = new System.Drawing.Point(276, 76);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(252, 23);
+            this.textBox1.TabIndex = 12;
             // 
             // solicitaranticipo
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1335, 850);
+            this.ClientSize = new System.Drawing.Size(890, 552);
             this.Controls.Add(this.Gpb_solicitaranticipo);
             this.Controls.Add(this.Pnl_Encabezado);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "solicitaranticipo";
             this.Text = "solicitaranticipo";
             this.Pnl_Encabezado.ResumeLayout(false);
@@ -224,10 +214,10 @@ namespace Capa_Vista_Anticipos
         private System.Windows.Forms.TextBox Txt_motivo;
         private System.Windows.Forms.TextBox Txt_monto;
         private System.Windows.Forms.DateTimePicker Dtp_fecha;
-        private System.Windows.Forms.ComboBox Cbo_Nombreempleados;
         private System.Windows.Forms.Label Lbl_motivo;
         private System.Windows.Forms.Label Lbl_monto;
         private System.Windows.Forms.Label Lbl_fecha;
         private System.Windows.Forms.Label Lbl_Nombreempleado;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
