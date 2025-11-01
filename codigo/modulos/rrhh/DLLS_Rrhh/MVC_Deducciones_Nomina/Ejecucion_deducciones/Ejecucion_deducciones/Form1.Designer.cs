54d7b2fe204226c0ed822c5a3c05cec5fc8af57d
DanielReyes03 <daniel_reyes2003@outlook.com>
2025-10-31 22:15:33 -0600

    modificando el mvc de deducciones

---
+++ codigo/modulos/rrhh/DLLS_Rrhh/MVC_Deducciones_Nomina/Ejecucion_deducciones/Ejecucion_deducciones/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿
+namespace Ejecucion_deducciones
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.frm_Deducciones_Nomina11 = new Capa_Vista_Deducciones_Nomina.Frm_Deducciones_Nomina1();
+            this.SuspendLayout();
+            // 
+            // frm_Deducciones_Nomina11
+            // 
+            this.frm_Deducciones_Nomina11.Location = new System.Drawing.Point(1, 12);
+            this.frm_Deducciones_Nomina11.Name = "frm_Deducciones_Nomina11";
+            this.frm_Deducciones_Nomina11.Size = new System.Drawing.Size(813, 451);
+            this.frm_Deducciones_Nomina11.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(794, 450);
+            this.Controls.Add(this.frm_Deducciones_Nomina11);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Capa_Vista_Deducciones_Nomina.Frm_Deducciones_Nomina1 frm_Deducciones_Nomina11;
+    }
+}
+
