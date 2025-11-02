75565c726e183d0129c8b9c059386725d82009eb
DanielReyes03 <daniel_reyes2003@outlook.com>
2025-11-02 15:12:06 -0600

    corrigiendo main

---
+++ codigo/modulos/rrhh/DLLS_Rrhh/MVC_Mantenimiento_Puestos_Nomina/Mantenimiento_Puestos/Mantenimiento_Puestos/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mantenimiento_Puestos
+{
+    class Program
+    {
+        static void Main() { 
+            
+        }
+    }
+}
