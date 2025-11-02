02aee5ff1e8d38c4f12c3c91d87d04ded2c26594
Leticia Sontay <lsontayc1@miumg.edu.gt>
2025-11-02 14:34:16 -0600

    Se crean forms para agregar y visualizar tipo de cambio

--- codigo/modulos/bancos/DLLS_Bancos/ProyFinal/Capa_Vista_TipoDeCambio/Frm_IngresoTipoDeCambio.cs
+++ codigo/modulos/bancos/DLLS_Bancos/ProyFinal/Capa_Vista_TipoDeCambio/Frm_IngresoTipoDeCambio.cs
@@ -10,14 +10,19 @@ using System.Windows.Forms;
 
 namespace Capa_Vista_TipoDeCambio
 {
-    public partial class Frm_TipoDeCambio : Form
+    public partial class Frm_IngresoTipoDeCambio : Form
     {
-        public Frm_TipoDeCambio()
+        public Frm_IngresoTipoDeCambio()
         {
             InitializeComponent();
         }
 
-        private void Frm_TipoDeCambio_Load(object sender, EventArgs e)
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
         {
 
         }
