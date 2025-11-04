1e3477492ad6470eb336a9ecce441e75510b6fb5
DanielReyes03 <daniel_reyes2003@outlook.com>
2025-11-03 22:06:00 -0600

    agregando mantenimiento y pantallas

---
+++ codigo/modulos/rrhh/Rrhh_MVC/SeguridadMVC/CapaVista/Frm_Deducciones_Nomina.Designer.cs
@@ -0,0 +1,58 @@
+﻿
+namespace Capa_Vista_Seguridad
+{
+    partial class Frm_Deducciones_Nomina
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.frm_Deducciones_Nomina1 = new Capa_Vista_Deducciones_Nomina.Frm_Deducciones_Nomina();
+            this.SuspendLayout();
+            // 
+            // frm_Deducciones_Nomina1
+            // 
+            this.frm_Deducciones_Nomina1.Location = new System.Drawing.Point(-3, 12);
+            this.frm_Deducciones_Nomina1.Name = "frm_Deducciones_Nomina1";
+            this.frm_Deducciones_Nomina1.Size = new System.Drawing.Size(813, 451);
+            this.frm_Deducciones_Nomina1.TabIndex = 0;
+            // 
+            // Frm_Deducciones_Nomina
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.frm_Deducciones_Nomina1);
+            this.Name = "Frm_Deducciones_Nomina";
+            this.Text = "Frm_Deducciones_Nomina";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Capa_Vista_Deducciones_Nomina.Frm_Deducciones_Nomina frm_Deducciones_Nomina1;
+    }
+}
