e665e413abc135f88810dc50c3604a07c896c487
Anderson <atriguerosm3@miumg.edu.gt>
2025-11-09 14:34:04 -0600

    Entrega 90% Implosion y Explosion

--- codigo/modulos/hoteleria/Modulos/Gestión de Salones/Implosion y Explosión de Materiales - Anderson Trigueros/IEM/Capa_Controlador_IE/Cls_Controlador_IE.cs
+++ codigo/modulos/hoteleria/Modulos/Gestión de Salones/Implosion y Explosión de Materiales - Anderson Trigueros/IEM/Capa_Controlador_IE/Cls_Controlador_IE.cs
@@ -12,25 +12,39 @@ namespace Capa_Controlador_IE
     {
         Cls_Sentencias_IE sentencias = new Cls_Sentencias_IE();
 
-        public DataTable DatosMenu()
+        public DataTable fun_DatosMenu()
         {
             return sentencias.fun_ObtenerMenus();
         }
 
-        public DataTable DatosReceta(int codigoMenu)
+        public DataTable fun_DatosReceta(int codigoMenu)
         {
             return sentencias.fun_ObtenerReceta(codigoMenu);
         }
 
-        public List<(string sCodigo, string sIngrediente, double doStock, string sUnidad)> verificarInventario(List<string> ingredientes)
+        public List<(string sCodigo, string sIngrediente, double doStock, string sUnidad)> fun_verificarInventario(List<string> ingredientes)
         {
-            return sentencias.ConsultarInventario(ingredientes);
-        }
-        
-        public void GenerarOrdenCompra(List<(int iCodigo, double doCantidad)> Listado)
-        {
-            sentencias.GuardarOrdenCompra(Listado);
+            return sentencias.fun_ConsultarInventario(ingredientes);
         }
 
+        public void pro_GenerarOrdenCompra(List<(int iCodigo, double doCantidad)> Listado)
+        {
+            sentencias.pro_GuardarOrdenCompra(Listado);
+        }
+
+        public DataTable fun_DatosOrdenes()
+        {
+            return sentencias.fun_ObtenerOrdenes();
+        }
+
+        public void pro_ActualizarCantidad(int iCodigoDetalle, double doCantidadNueva)
+        {
+            sentencias.pro_ActualizarCantidad(iCodigoDetalle, doCantidadNueva);
+        }
+
+        public void pro_EliminarOrden(int iCodigoDetalle)
+        {
+            sentencias.pro_EliminarOrden(iCodigoDetalle);
+        }
     }
 } 
