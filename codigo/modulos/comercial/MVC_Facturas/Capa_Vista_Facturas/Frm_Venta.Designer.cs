592a4e2639db4f89b02ac6523872bacfa0ce8f45
Juanka20-Sandoval00 <jsandovalq@miumg.edu.gt>
2025-11-13 16:29:58 -0600

    Cambio en tamaño de botones Frm_Venta

--- codigo/modulos/comercial/MVC_Facturas/Capa_Vista_Facturas/Frm_Venta.Designer.cs
+++ codigo/modulos/comercial/MVC_Facturas/Capa_Vista_Facturas/Frm_Venta.Designer.cs
@@ -85,17 +85,17 @@ namespace Capa_Vista_Facturas
             this.Pnl_Producto.Controls.Add(this.Lbl_Codigo);
             this.Pnl_Producto.Controls.Add(this.Lbl_Producto);
             this.Pnl_Producto.Controls.Add(this.Dgv_Lista);
-            this.Pnl_Producto.Location = new System.Drawing.Point(50, 108);
-            this.Pnl_Producto.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Pnl_Producto.Location = new System.Drawing.Point(50, 85);
+            this.Pnl_Producto.Margin = new System.Windows.Forms.Padding(4);
             this.Pnl_Producto.Name = "Pnl_Producto";
-            this.Pnl_Producto.Size = new System.Drawing.Size(699, 712);
+            this.Pnl_Producto.Size = new System.Drawing.Size(699, 604);
             this.Pnl_Producto.TabIndex = 0;
             // 
             // Btn_Eliminar
             // 
             this.Btn_Eliminar.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.Btn_Eliminar.Location = new System.Drawing.Point(476, 182);
-            this.Btn_Eliminar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Btn_Eliminar.Location = new System.Drawing.Point(476, 152);
+            this.Btn_Eliminar.Margin = new System.Windows.Forms.Padding(4);
             this.Btn_Eliminar.Name = "Btn_Eliminar";
             this.Btn_Eliminar.Size = new System.Drawing.Size(181, 74);
             this.Btn_Eliminar.TabIndex = 18;
@@ -105,8 +105,8 @@ namespace Capa_Vista_Facturas
             // Btn_Agregar
             // 
             this.Btn_Agregar.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.Btn_Agregar.Location = new System.Drawing.Point(476, 88);
-            this.Btn_Agregar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Btn_Agregar.Location = new System.Drawing.Point(476, 60);
+            this.Btn_Agregar.Margin = new System.Windows.Forms.Padding(4);
             this.Btn_Agregar.Name = "Btn_Agregar";
             this.Btn_Agregar.Size = new System.Drawing.Size(181, 74);
             this.Btn_Agregar.TabIndex = 17;
@@ -115,16 +115,16 @@ namespace Capa_Vista_Facturas
             // 
             // Txt_Efectivo
             // 
-            this.Txt_Efectivo.Location = new System.Drawing.Point(291, 614);
-            this.Txt_Efectivo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Txt_Efectivo.Location = new System.Drawing.Point(280, 541);
+            this.Txt_Efectivo.Margin = new System.Windows.Forms.Padding(4);
             this.Txt_Efectivo.Name = "Txt_Efectivo";
             this.Txt_Efectivo.Size = new System.Drawing.Size(124, 27);
             this.Txt_Efectivo.TabIndex = 16;
             // 
             // Txt_IngresoCantidad
             // 
-            this.Txt_IngresoCantidad.Location = new System.Drawing.Point(240, 230);
-            this.Txt_IngresoCantidad.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Txt_IngresoCantidad.Location = new System.Drawing.Point(240, 209);
+            this.Txt_IngresoCantidad.Margin = new System.Windows.Forms.Padding(4);
             this.Txt_IngresoCantidad.Name = "Txt_IngresoCantidad";
             this.Txt_IngresoCantidad.Size = new System.Drawing.Size(124, 27);
             this.Txt_IngresoCantidad.TabIndex = 15;
@@ -139,8 +139,8 @@ namespace Capa_Vista_Facturas
             "Bebidas",
             "Licores",
             "Postres"});
-            this.Cbo_Producto.Location = new System.Drawing.Point(229, 44);
-            this.Cbo_Producto.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Cbo_Producto.Location = new System.Drawing.Point(229, 22);
+            this.Cbo_Producto.Margin = new System.Windows.Forms.Padding(4);
             this.Cbo_Producto.Name = "Cbo_Producto";
             this.Cbo_Producto.Size = new System.Drawing.Size(234, 28);
             this.Cbo_Producto.TabIndex = 14;
@@ -148,7 +148,7 @@ namespace Capa_Vista_Facturas
             // Lbl_IngresoDevolucion
             // 
             this.Lbl_IngresoDevolucion.AutoSize = true;
-            this.Lbl_IngresoDevolucion.Location = new System.Drawing.Point(539, 614);
+            this.Lbl_IngresoDevolucion.Location = new System.Drawing.Point(533, 541);
             this.Lbl_IngresoDevolucion.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.Lbl_IngresoDevolucion.Name = "Lbl_IngresoDevolucion";
             this.Lbl_IngresoDevolucion.Size = new System.Drawing.Size(15, 20);
@@ -159,7 +159,7 @@ namespace Capa_Vista_Facturas
             // 
             this.Lbl_Devolucion.AutoSize = true;
             this.Lbl_Devolucion.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Lbl_Devolucion.Location = new System.Drawing.Point(488, 568);
+            this.Lbl_Devolucion.Location = new System.Drawing.Point(487, 505);
             this.Lbl_Devolucion.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.Lbl_Devolucion.Name = "Lbl_Devolucion";
             this.Lbl_Devolucion.Size = new System.Drawing.Size(113, 17);
@@ -170,7 +170,7 @@ namespace Capa_Vista_Facturas
             // 
             this.Lbl_Efectivo.AutoSize = true;
             this.Lbl_Efectivo.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Lbl_Efectivo.Location = new System.Drawing.Point(300, 568);
+            this.Lbl_Efectivo.Location = new System.Drawing.Point(305, 505);
             this.Lbl_Efectivo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.Lbl_Efectivo.Name = "Lbl_Efectivo";
             this.Lbl_Efectivo.Size = new System.Drawing.Size(88, 17);
@@ -180,7 +180,7 @@ namespace Capa_Vista_Facturas
             // Lbl_TotalPagar
             // 
             this.Lbl_TotalPagar.AutoSize = true;
-            this.Lbl_TotalPagar.Location = new System.Drawing.Point(152, 614);
+            this.Lbl_TotalPagar.Location = new System.Drawing.Point(151, 541);
             this.Lbl_TotalPagar.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.Lbl_TotalPagar.Name = "Lbl_TotalPagar";
             this.Lbl_TotalPagar.Size = new System.Drawing.Size(15, 20);
@@ -191,7 +191,7 @@ namespace Capa_Vista_Facturas
             // 
             this.Lbl_Total.AutoSize = true;
             this.Lbl_Total.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Lbl_Total.Location = new System.Drawing.Point(96, 568);
+            this.Lbl_Total.Location = new System.Drawing.Point(96, 505);
             this.Lbl_Total.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.Lbl_Total.Name = "Lbl_Total";
             this.Lbl_Total.Size = new System.Drawing.Size(137, 17);
@@ -202,7 +202,7 @@ namespace Capa_Vista_Facturas
             // 
             this.Lbl_Cantidad.AutoSize = true;
             this.Lbl_Cantidad.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Lbl_Cantidad.Location = new System.Drawing.Point(96, 232);
+            this.Lbl_Cantidad.Location = new System.Drawing.Point(96, 209);
             this.Lbl_Cantidad.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.Lbl_Cantidad.Name = "Lbl_Cantidad";
             this.Lbl_Cantidad.Size = new System.Drawing.Size(90, 17);
@@ -212,7 +212,7 @@ namespace Capa_Vista_Facturas
             // Lbl_IngresoNombre
             // 
             this.Lbl_IngresoNombre.AutoSize = true;
-            this.Lbl_IngresoNombre.Location = new System.Drawing.Point(236, 141);
+            this.Lbl_IngresoNombre.Location = new System.Drawing.Point(236, 117);
             this.Lbl_IngresoNombre.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.Lbl_IngresoNombre.Name = "Lbl_IngresoNombre";
             this.Lbl_IngresoNombre.Size = new System.Drawing.Size(15, 20);
@@ -222,7 +222,7 @@ namespace Capa_Vista_Facturas
             // Lbl_IngresoCodigo
             // 
             this.Lbl_IngresoCodigo.AutoSize = true;
-            this.Lbl_IngresoCodigo.Location = new System.Drawing.Point(236, 95);
+            this.Lbl_IngresoCodigo.Location = new System.Drawing.Point(236, 76);
             this.Lbl_IngresoCodigo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.Lbl_IngresoCodigo.Name = "Lbl_IngresoCodigo";
             this.Lbl_IngresoCodigo.Size = new System.Drawing.Size(15, 20);
@@ -232,7 +232,7 @@ namespace Capa_Vista_Facturas
             // Lbl_IngresoPrecio
             // 
             this.Lbl_IngresoPrecio.AutoSize = true;
-            this.Lbl_IngresoPrecio.Location = new System.Drawing.Point(236, 186);
+            this.Lbl_IngresoPrecio.Location = new System.Drawing.Point(236, 161);
             this.Lbl_IngresoPrecio.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.Lbl_IngresoPrecio.Name = "Lbl_IngresoPrecio";
             this.Lbl_IngresoPrecio.Size = new System.Drawing.Size(15, 20);
@@ -243,7 +243,7 @@ namespace Capa_Vista_Facturas
             // 
             this.Lbl_Precio.AutoSize = true;
             this.Lbl_Precio.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Lbl_Precio.Location = new System.Drawing.Point(96, 182);
+            this.Lbl_Precio.Location = new System.Drawing.Point(96, 161);
             this.Lbl_Precio.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.Lbl_Precio.Name = "Lbl_Precio";
             this.Lbl_Precio.Size = new System.Drawing.Size(70, 17);
@@ -254,7 +254,7 @@ namespace Capa_Vista_Facturas
             // 
             this.Lbl_Nombre.AutoSize = true;
             this.Lbl_Nombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Lbl_Nombre.Location = new System.Drawing.Point(96, 134);
+            this.Lbl_Nombre.Location = new System.Drawing.Point(96, 117);
             this.Lbl_Nombre.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.Lbl_Nombre.Name = "Lbl_Nombre";
             this.Lbl_Nombre.Size = new System.Drawing.Size(79, 17);
@@ -265,7 +265,7 @@ namespace Capa_Vista_Facturas
             // 
             this.Lbl_Codigo.AutoSize = true;
             this.Lbl_Codigo.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Lbl_Codigo.Location = new System.Drawing.Point(96, 91);
+            this.Lbl_Codigo.Location = new System.Drawing.Point(96, 76);
             this.Lbl_Codigo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.Lbl_Codigo.Name = "Lbl_Codigo";
             this.Lbl_Codigo.Size = new System.Drawing.Size(74, 17);
@@ -276,7 +276,7 @@ namespace Capa_Vista_Facturas
             // 
             this.Lbl_Producto.AutoSize = true;
             this.Lbl_Producto.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Lbl_Producto.Location = new System.Drawing.Point(96, 44);
+            this.Lbl_Producto.Location = new System.Drawing.Point(96, 28);
             this.Lbl_Producto.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.Lbl_Producto.Name = "Lbl_Producto";
             this.Lbl_Producto.Size = new System.Drawing.Size(100, 17);
@@ -296,14 +296,14 @@ namespace Capa_Vista_Facturas
             this.Column3,
             this.Column4,
             this.Column5});
-            this.Dgv_Lista.Location = new System.Drawing.Point(32, 292);
-            this.Dgv_Lista.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Dgv_Lista.Location = new System.Drawing.Point(32, 258);
+            this.Dgv_Lista.Margin = new System.Windows.Forms.Padding(4);
             this.Dgv_Lista.Name = "Dgv_Lista";
             this.Dgv_Lista.ReadOnly = true;
             this.Dgv_Lista.RowHeadersVisible = false;
             this.Dgv_Lista.RowHeadersWidth = 51;
             this.Dgv_Lista.RowTemplate.Height = 24;
-            this.Dgv_Lista.Size = new System.Drawing.Size(644, 232);
+            this.Dgv_Lista.Size = new System.Drawing.Size(644, 212);
             this.Dgv_Lista.TabIndex = 0;
             // 
             // Column1
@@ -345,8 +345,8 @@ namespace Capa_Vista_Facturas
             // 
             this.Btn_Vender.BackColor = System.Drawing.Color.White;
             this.Btn_Vender.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.Btn_Vender.Location = new System.Drawing.Point(271, 841);
-            this.Btn_Vender.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Btn_Vender.Location = new System.Drawing.Point(279, 697);
+            this.Btn_Vender.Margin = new System.Windows.Forms.Padding(4);
             this.Btn_Vender.Name = "Btn_Vender";
             this.Btn_Vender.Size = new System.Drawing.Size(271, 80);
             this.Btn_Vender.TabIndex = 1;
@@ -359,12 +359,12 @@ namespace Capa_Vista_Facturas
             this.Lbl_Factura.BackColor = System.Drawing.Color.Silver;
             this.Lbl_Factura.Font = new System.Drawing.Font("Rockwell", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Lbl_Factura.ForeColor = System.Drawing.Color.Black;
-            this.Lbl_Factura.Location = new System.Drawing.Point(270, 31);
+            this.Lbl_Factura.Location = new System.Drawing.Point(349, 31);
             this.Lbl_Factura.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.Lbl_Factura.Name = "Lbl_Factura";
-            this.Lbl_Factura.Size = new System.Drawing.Size(218, 50);
+            this.Lbl_Factura.Size = new System.Drawing.Size(164, 50);
             this.Lbl_Factura.TabIndex = 2;
-            this.Lbl_Factura.Text = "FACTURA";
+            this.Lbl_Factura.Text = "VENTA";
             // 
             // menuStrip1
             // 
@@ -375,14 +375,14 @@ namespace Capa_Vista_Facturas
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
             this.menuStrip1.Padding = new System.Windows.Forms.Padding(10, 2, 0, 2);
-            this.menuStrip1.Size = new System.Drawing.Size(801, 30);
+            this.menuStrip1.Size = new System.Drawing.Size(801, 28);
             this.menuStrip1.TabIndex = 3;
             this.menuStrip1.Text = "menuStrip1";
             // 
             // listadoDeFacturaToolStripMenuItem
             // 
             this.listadoDeFacturaToolStripMenuItem.Name = "listadoDeFacturaToolStripMenuItem";
-            this.listadoDeFacturaToolStripMenuItem.Size = new System.Drawing.Size(165, 26);
+            this.listadoDeFacturaToolStripMenuItem.Size = new System.Drawing.Size(165, 24);
             this.listadoDeFacturaToolStripMenuItem.Text = "Listado de Factura";
             this.listadoDeFacturaToolStripMenuItem.Click += new System.EventHandler(this.listadoDeFacturaToolStripMenuItem_Click);
             // 
@@ -391,7 +391,7 @@ namespace Capa_Vista_Facturas
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Silver;
-            this.ClientSize = new System.Drawing.Size(801, 936);
+            this.ClientSize = new System.Drawing.Size(801, 825);
             this.Controls.Add(this.Lbl_Factura);
             this.Controls.Add(this.Btn_Vender);
             this.Controls.Add(this.Pnl_Producto);
@@ -399,7 +399,7 @@ namespace Capa_Vista_Facturas
             this.Font = new System.Drawing.Font("Rockwell", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
             this.MainMenuStrip = this.menuStrip1;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Frm_Venta";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Hotel San Carlos";
